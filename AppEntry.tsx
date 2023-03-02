3accf5326c4303366153163fe05c74eeb68f12fc
Massimo Persic <persic.massimo@gmail.com>
2023-03-03 00:52:31 +0100

    persist navigation state

--- AppEntry.tsx
+++ AppEntry.tsx
@@ -1,15 +1,77 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Router} from './src/navigation/Router';
 import {AuthUserProvider} from './src/hooks/useAuth';
 import {QueryClientProvider} from './src/service/http/react-query/queryClient';
+import {Screen} from './src/components/Screen';
+import {View} from './src/components/View';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {ActivityIndicator, Linking, Platform} from 'react-native';
 
 function AppEntry() {
+  const NAVIGATION_PERSISTENCE_KEY = 'NAVIGATION_PERSISTENCE';
+
+  const [isAppReady, setIsAppReady] = useState(false);
+
+  const [isReady, setIsReady] = useState(false);
+  const [initialState, setInitialState] = useState();
+
+  useEffect(() => {
+    const shouldPersistNavigation = true;
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+
+        if (
+          Platform.OS !== 'web' &&
+          initialUrl == null &&
+          shouldPersistNavigation
+        ) {
+          // Only restore state if there's no deep link and we're not on web
+          const savedStateString = await AsyncStorage.getItem(
+            NAVIGATION_PERSISTENCE_KEY,
+          );
+          const state = savedStateString
+            ? JSON.parse(savedStateString)
+            : undefined;
+
+          if (state !== undefined) {
+            setInitialState(state);
+          }
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) {
+      restoreState();
+      setIsAppReady(true);
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return <ActivityIndicator />;
+  }
   return (
     <SafeAreaProvider>
       <QueryClientProvider>
         <AuthUserProvider>
-          <Router />
+          {isAppReady ? (
+            <Router
+              initialState={initialState}
+              onStateChange={(state: any) =>
+                AsyncStorage.setItem(
+                  NAVIGATION_PERSISTENCE_KEY,
+                  JSON.stringify(state),
+                )
+              }
+            />
+          ) : (
+            <Screen>
+              <View flex backgroundColorTheme />
+            </Screen>
+          )}
         </AuthUserProvider>
       </QueryClientProvider>
     </SafeAreaProvider>
--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -1,15 +1,27 @@
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  InitialState,
+  NavigationContainer,
+  NavigationState,
+} from '@react-navigation/native';
 import React from 'react';
 import useAuth from '../hooks/useAuth';
 import {useSilentLogin} from '../hooks/useSilentLogin';
 import {AuthStack} from './AuthStack';
 import {RootStack} from './RootStack';
 
-export const Router = () => {
+export const Router = ({
+  initialState,
+  onStateChange,
+}: {
+  initialState: InitialState | undefined;
+  onStateChange: (state: NavigationState | undefined) => void;
+}) => {
   const {isLoggedIn} = useAuth();
   useSilentLogin();
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      initialState={initialState}
+      onStateChange={onStateChange}>
       {isLoggedIn ? <RootStack /> : <AuthStack />}
     </NavigationContainer>
   );
