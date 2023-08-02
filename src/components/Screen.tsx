d320cdd8c812430c3db84dd9d87f822e9fbcbcb9
Massimo Persic <persic.massimo@gmail.com>
2023-08-03 00:39:08 +0200

    bug with safeAreaView

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {KeyboardAvoidingView, ScrollView, StyleSheet} from 'react-native';
-import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {ErrorView} from './ErrorView';
 import {LoadingView} from './LoadingView';
 import {View} from './View';
@@ -50,35 +50,33 @@ export const Screen = ({
     </>
   );
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <KeyboardAvoidingView
-        style={styles.container}
-        keyboardVerticalOffset={-(insets.bottom + insets.top)}
-        behavior={'padding'}>
-        {withList ? (
-          <View
-            style={[
-              styles.flexGrow,
-              {
-                paddingTop: withTopInsets ? insets.top : 0,
-                paddingBottom: withBottomInsets ? insets.bottom : 0,
-              },
-            ]}>
-            {content}
-          </View>
-        ) : (
-          <ScrollView
-            contentContainerStyle={[styles.flexGrow]}
-            style={{
+    <KeyboardAvoidingView
+      style={styles.container}
+      keyboardVerticalOffset={-(insets.bottom + insets.top)}
+      behavior={'padding'}>
+      {withList ? (
+        <View
+          style={[
+            styles.flexGrow,
+            {
               paddingTop: withTopInsets ? insets.top : 0,
               paddingBottom: withBottomInsets ? insets.bottom : 0,
-            }}
-            keyboardShouldPersistTaps={'never'}
-            bounces={!preventScroll}>
-            {content}
-          </ScrollView>
-        )}
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+            },
+          ]}>
+          {content}
+        </View>
+      ) : (
+        <ScrollView
+          contentContainerStyle={[styles.flexGrow]}
+          style={{
+            paddingTop: withTopInsets ? insets.top : 0,
+            paddingBottom: withBottomInsets ? insets.bottom : 0,
+          }}
+          keyboardShouldPersistTaps={'never'}
+          bounces={!preventScroll}>
+          {content}
+        </ScrollView>
+      )}
+    </KeyboardAvoidingView>
   );
 };
